95cb6897c9977751d8599bda8bf2c37d8b2dd6df
taylorvicente <150193582+taylorvicente@users.noreply.github.com>
2024-06-12 19:01:46 -0700

    fixed some small bugs

--- background_infrastructure.js
+++ background_infrastructure.js
@@ -25,12 +25,11 @@ document.addEventListener('scroll', function() {
     const time = document.querySelector('.left-items .time');
     const location = document.querySelector('.left-items .location');
     const scrollPosition = window.scrollY; // Current scroll position from the top of the page
-    
+
     // Apply transformation based on the scroll position
-    // Example: Move right-bar up as user scrolls down
     rightBar.style.transform = `translateY(${scrollPosition * 0.5}px)`;
-    
-    // Optional: Fade out the right-bar as user scrolls down
+
+    // Fade out the right-bar as user scrolls down
     const fadeSpeed = 250; // Adjust the fade speed as needed
     const rightBarOpacity = Math.max(1 - scrollPosition / fadeSpeed, 0);
     const topbarborder = Math.max(1 - scrollPosition / 215, 0);
@@ -43,9 +42,8 @@ document.addEventListener('scroll', function() {
     const bottomBar = document.querySelector('.bottom-bar');
     const totalPageHeight = document.body.scrollHeight - window.innerHeight;
     const scrollPercentage = (scrollPosition / totalPageHeight) * 100;
-    
-    // Linear opacity transition from 0 to 1 as scroll percentage goes from 95 to 100
-    if (scrollPercentage >= 95 && scrollPercentage <= 100) {
+
+    if (scrollPercentage >= 96) {
         const opacity = (scrollPercentage - 95) / 5; // Calculate opacity from 0 to 1
         bottomBar.style.opacity = opacity.toString();
         bottomBar.style.visibility = 'visible';
