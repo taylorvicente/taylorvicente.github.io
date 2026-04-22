880c823288acd85fffa7a7cc405ad834eee09606
taylorvicente <150193582+taylorvicente@users.noreply.github.com>
2026-04-22 12:36:32 -0400

    fixed the all white screen

---
+++ dist/js/portfolio-config.js
@@ -0,0 +1,10 @@
+window.PORTFOLIO_CONFIG = {
+  display_name: "Taylor Vicente",
+  nav_brand: "TAYLOR_VICENTE",
+  contact_email: "taylorvicent03@gmain.com",
+  social_linkedin: "https://www.linkedin.com/in/taylorvicente/",
+  social_github: "https://github.com/tvicent",
+  social_extra_label: "INSTITUTE_OF_MECHANICAL_ENGINEERS",
+  social_extra_href: "",
+  copyright_year: 2026,
+};
