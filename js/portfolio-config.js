ff57eacb9e847da9aaae7607a50af886fd868da6
taylorvicente <150193582+taylorvicente@users.noreply.github.com>
2026-04-23 13:01:47 -0400

    updated contact info

--- js/portfolio-config.js
+++ js/portfolio-config.js
@@ -6,11 +6,11 @@ window.PORTFOLIO_CONFIG = {
   display_name: 'Taylor Vicente',
   /** Shown in the nav bar and footer brand line (underscore style is fine). */
   nav_brand: 'TAYLOR_VICENTE',
-  contact_email: 'taylorvicent03@gmain.com',
+  contact_email: 'taylorvicent03@gmail.com',
   social_linkedin: 'https://www.linkedin.com/in/taylorvicente/',
   social_github: 'https://github.com/tvicent',
   /** Optional fourth footer link; leave href '' to keep a placeholder # link. */
-  social_extra_label: 'INSTITUTE_OF_MECHANICAL_ENGINEERS',
+  social_extra_label: 'Purdue University School of Mechanical Engineering',
   social_extra_href: '',
   copyright_year: 2026,
 };
